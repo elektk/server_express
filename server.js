609a20b848e4551d5bed0329c7ac828b56e7c1c0
tka4 <suportenik@gmail.com>
2024-04-12 12:14:32 +0300

    update server

--- server.js
+++ server.js
@@ -1,27 +1,62 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const headphonesArray = require('./db/headphones-list.json')
+const cart = require('./db/cart.json')
+const path = require('path')
+const fs = require('fs')
 
 const PORT = 8000
+const DB = path.join('db', 'cart.json')
 
-app.get('/headphones', (req, res) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-    const cardInfoTopPicksList = [{
-        cardTitle: "Беспроводные наушники №1",
-        reviews: 100,
-        price:32
-    },{
-        cardTitle: "Беспроводные наушники №2",
-        reviews: 130,
-        price:95
-    },{
-        cardTitle: "Беспроводные наушники №3",
-        reviews: 88,
-        price:132
-    }];
-    res.send(cardInfoTopPicksList)
+let sent = false;
+
+app.use(cors())
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+
+app.get('/headphones/get-list', (req, res) => {
+    res.status(200)
+    res.send(headphonesArray)
   })
+
+app.get('/cart/get', (req, res)=>{
+    res.status(200)
+    res.send(cart)
+})
+
+app.post('/cart/add', (req, res)=>{
+  const postData = req.body
+  res.setHeader = function(name, value) {
+    if (!sent) {
+      res.setHeader(name, value);
+    }
+  }
+  sent = true;
+  if(!postData){
+    return res.status(400).send('Bad request')
+  }
+
+  fs.readFile(DB, (err, data)=> {
+    if (err) throw err;
+
+    try {
+      let cartData = JSON.stringify([ ...JSON.parse(data), postData])
+      fs.writeFile(DB, cartData, (writeErr)=>{
+        if (writeErr) {
+          return res.status(500).send('Не удалось произвести запись')
+        }
+        res.status(200).send('Успешно добавлен товар в корзину')
+      })
+    } catch {
+      return res.status(500).send('Не удалось произвести запись')
+    }
+  })
+
+    res.status(200)
+    res.send()
+})
 
 app.listen(PORT, () => {
     console.log(`Сервер запущен на порту ${PORT}`)
